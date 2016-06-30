d02e6341f917d2f27fb1706db95204e9a988caa9
Emil Apelgren <emilapelgren@outlook.com>
2016-06-30 13:01:46 +0200

    removed script bc. bots abusing TC

--- testCloudScript.sh
+++ testCloudScript.sh
@@ -1,1 +1,1 @@
-mono packages/Xamarin.UITest.1.3.9/tools/test-cloud.exe submit xemil.apk 441f384365c1af2176c5bc971d82bb0c --devices 65c0f733 --series "master" --locale "en_US" --user emilapelgren@hotmail.com --assembly-dir ./UITests/bin/Debug/
+
